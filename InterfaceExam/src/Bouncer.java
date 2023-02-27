d9f28e537268719584fb4e948f7c592490ff8233
JungSoy <jungsoyy@gmail.com>
2023-02-27 17:53:41 +0900

    Initial commit

---
+++ InterfaceExam/src/Bouncer.java
@@ -0,0 +1,15 @@
+
+public class Bouncer {
+	
+	void barkAnimal(Barkable a) {
+		a.bark();
+	}
+
+//	void barkAnimal(Animal animal) {
+//		if(animal instanceof Tiger) {
+//			System.out.println("어흥");
+//		} else if(animal instanceof Lion) {
+//			System.out.println("으르렁");
+//		}
+//	}
+}
