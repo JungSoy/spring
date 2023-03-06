d903f330fd0badaffd8085fca1d757c718d97fdb
JungSoy <jungsoyy@gmail.com>
2023-03-06 18:09:42 +0900

    20230306

---
+++ CarShop/src/com/carshop/controller/CarDTO.java
@@ -0,0 +1,74 @@
+package com.carshop.controller;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class CarDTO {
+	
+	private String cid, cname, cprice, ccate, cdesc;
+	private MultipartFile carimage;
+
+	public String getCid() {
+		return cid;
+	}
+
+	public void setCid(String cid) {
+		this.cid = cid;
+	}
+
+	public String getCname() {
+		return cname;
+	}
+
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+
+	public String getCprice() {
+		return cprice;
+	}
+
+	public void setCprice(String cprice) {
+		this.cprice = cprice;
+	}
+
+	public String getCcate() {
+		return ccate;
+	}
+
+	public void setCcate(String ccate) {
+		this.ccate = ccate;
+	}
+
+	public String getCdesc() {
+		return cdesc;
+	}
+
+	public void setCdesc(String cdesc) {
+		this.cdesc = cdesc;
+	}
+
+	public MultipartFile getCarimage() {
+		return carimage;
+	}
+
+	public void setCarimage(MultipartFile carimage) {
+		this.carimage = carimage;
+	}
+
+	public CarDTO(String cid, String cname, String cprice, String ccate, String cdesc) {
+
+		this.cid = cid;
+		this.cname = cname;
+		this.cprice = cprice;
+		this.ccate = ccate;
+		this.cdesc = cdesc;
+		this.carimage = carimage;
+	}
+
+	public CarDTO() {
+
+	}
+	
+	
+
+}
---
+++ CarShop02/src/main/java/com/carshop/controller/CarController.java
@@ -0,0 +1,82 @@
+package com.carshop.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CarController {
+
+	@Autowired
+	private CarService carService;
+	
+	@RequestMapping("/cars")
+	public String CarList(Model model) {
+		List<CarDTO> list = carService.getAllCarList();
+		model.addAttribute("carList", list);
+		return "cars";
+	}
+	
+	@GetMapping("/{ccate}")
+	public String requestCarsByCategory(@PathVariable("ccate") String carCategory, Model model) {
+		List<CarDTO> carsByCategory = carService.getCarListByCategory(carCategory);
+		model.addAttribute("carList", carsByCategory);
+		return "cars";
+	}
+	
+	@GetMapping("/car")
+	public String requestCarById(@RequestParam("id") String carId, Model model) {
+		
+		CarDTO carById = carService.getCarById(carId);
+		model.addAttribute("car", carById);
+		
+		return "car";
+	}
+	
+	//입력받음
+	@GetMapping("/admin/add")	
+	public String requestAddCarForm(@ModelAttribute("NewCar") CarDTO car) {
+		
+		return "addCar";
+	}
+	
+	//입력받은 것 뿌려줌
+	@PostMapping("/admin/add")
+	public String submitAddNewCar(@ModelAttribute("NewCar") CarDTO car) {
+		carService.setNewCar(car);
+		
+		return "redirect:/cars";
+	}
+	
+	//model이 가는 곳마다 다 붙음 
+	@ModelAttribute
+	public void addAttributes(Model model) {
+		model.addAttribute("addTitle", "신규 차량 등록");
+	}
+	
+	@GetMapping("/login")	
+	public String loginMethod() {
+		
+		return "login";
+	}
+	
+	@GetMapping("/loginfailed")	
+	public String loginfailedMethod() {
+		
+		return "login";
+	}
+	
+	/*
+	 * @GetMapping("/logout") public String logoutMethod() {
+	 * 
+	 * return "logout"; }
+	 */
+}
